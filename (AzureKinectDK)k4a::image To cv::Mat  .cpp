320255df90cb11cfc400624c28503fecf0a2e450
UEBoy2019 <56669924+UEBoy2019@users.noreply.github.com>
2020-12-22 09:32:57 +0800

    Update (AzureKinectDK)k4a::image To cv::Mat  .cpp

--- (AzureKinectDK)k4a::image To cv::Mat  .cpp
+++ (AzureKinectDK)k4a::image To cv::Mat  .cpp
@@ -5,7 +5,8 @@ cv::Mat AzureKinectDevice::K4a2Mat(k4a::image rgb)
 	cv::Mat Color_frame = cv::Mat(hight, width, CV_8UC4, k4a_image_get_buffer(rgb.handle())).clone();
 	//if u use depth image , depth_image is just one channel ,and 16bit ;8 bit not to save it  ,and u should :
 	//first :cv::Mat Depth_frame = cv::Mat(hight, width, CV_16UC1, reinterpret_cast<uint16_t*>k4a_image_get_buffer(rgb.handle())).clone();
-	//second : convertTo(Depth_frame, CV_8UC1, 255.0 / 5000, 0.0);
+	//second :  Depth_frame.convertTo(Depth_frame, CV_8UC1, 255.0 / 5000, 0.0);
+	//third: return Depth_frame;
 	
 	return Color_frame;
 }
