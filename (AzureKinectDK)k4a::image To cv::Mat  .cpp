270aa9d3fd2dd47f1c44521626c510755038cb68
UEBoy2019 <56669924+UEBoy2019@users.noreply.github.com>
2020-12-22 09:39:50 +0800

    Update (AzureKinectDK)k4a::image To cv::Mat  .cpp

--- (AzureKinectDK)k4a::image To cv::Mat  .cpp
+++ (AzureKinectDK)k4a::image To cv::Mat  .cpp
@@ -1,12 +1,20 @@
+//k4a RGB image
 cv::Mat AzureKinectDevice::K4a2Mat(k4a::image rgb)
 {
 	int hight = k4a_image_get_height_pixels(rgb.handle());
 	int width = k4a_image_get_width_pixels(rgb.handle());
-	cv::Mat Color_frame = cv::Mat(hight, width, CV_8UC4, k4a_image_get_buffer(rgb.handle())).clone();
-	//if u use depth image , depth_image is just one channel ,and 16bit ;8 bit not to save it  ,and u should :
-	//first :cv::Mat Depth_frame = cv::Mat(hight, width, CV_16UC1, reinterpret_cast<uint16_t*>k4a_image_get_buffer(rgb.handle())).clone();
-	//second :  Depth_frame.convertTo(Depth_frame, CV_8UC1, 255.0 / 5000, 0.0);
-	//third: return Depth_frame;
-	
+	cv::Mat Color_frame = cv::Mat(hight, width, CV_8UC4, k4a_image_get_buffer(rgb.handle()));
 	return Color_frame;
+}
+
+//k4a depth image
+cv::Mat AzureKinectDevice::k4a2Mat_Depth(k4a::image indepth)
+{
+	int32 hight = indepth.get_width_pixels();
+	int32 width = indepth.get_height_pixels();
+	cv::Mat Depth_frame = cv::Mat(hight, width, CV_16UC1, reinterpret_cast<uint16_t*>(k4a_image_get_buffer(indepth.handle())));
+	Depth_frame.convertTo(Depth_frame, CV_8UC1, 255.0 / 5000, 0.0);
+	cv::Mat outDepth;
+	cv::cvtColor(Depth_frame, outDepth, CV_GRAY2RGBA);
+	return outDepth;
 }
