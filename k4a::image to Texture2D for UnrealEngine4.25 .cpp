43330b08556b2fd3d30f1e8c019e9f0f58cee2f9
VegetableWithChicken <865697378@qq.com>
2021-07-12 11:21:14 +0800

    Update k4a::image to Texture2D for UnrealEngine4.25 .cpp

--- k4a::image to Texture2D for UnrealEngine4.25 .cpp
+++ k4a::image to Texture2D for UnrealEngine4.25 .cpp
@@ -13,3 +13,30 @@ void AzureKinectDevice::k4a2TextureRGB(k4a::image inImg)
 	NewTexture->PlatformData->Mips[0].BulkData.Unlock();
 	NewTexture->UpdateResource();
 }
+
+
+//if 16 bit depth image,you should change the UTexture2D format like this  
+UTexture2D* AzureKinectDevice::CreateNewTexture(int w, int h, EPixelFormat InFormat)
+{
+	UTexture2D* tex = UTexture2D::CreateTransient(w, h, InFormat);
+	tex->AddressX = TextureAddress::TA_Mirror;
+	tex->AddressY = TextureAddress::TA_Mirror;
+	tex->Filter = TextureFilter::TF_Bilinear;
+	tex->RefreshSamplerStates();
+	tex->UpdateResource();
+	return tex;
+}
+
+void AzureKinectDevice::MakeTexture(const k4a::image& inImg)
+{
+	//if runtime update Utexture2D* ,you should create inMem as global var;
+	Utexture2D* inMem = CreateNewTexture((int32)1920, (int32)1080, EPixelFormat::PF_G16);//传输16bit的深度图
+	void* Datas = inMem->PlatformData->Mips[0].BulkData.Lock(LOCK_READ_WRITE);
+	FMemory::Memmove(Datas, inImg.get_buffer(), inImg.get_size());
+	inMem->PlatformData->Mips[0].BulkData.Unlock();
+	inMem->UpdateResource();
+	
+}
+
+
+
